3c5c798f2247939c230aada9db0f45e7d5c834dd
Diana <stels270799@gmail.com>
2021-12-09 17:47:56 +0300

    front is working

--- js/script.js
+++ js/script.js
@@ -1,48 +1,48 @@
 class Api {
   constructor(url, headers) {
-     this._url = url;
-     this._headers = headers;
+     this.url = url;
+     this.headers = headers;
   }
 
   getItems() {
-     return fetch(this._url, {
+     return fetch(this.url, {
         method: 'GET',
-        headers: this._headers
+        headers: this.headers
      }).then((res) => {
-        return this._processResult(res, 'Ошибка при получении данных');
+        return this.processResult(res, 'Ошибка при получении данных');
      });
   }
 
   deleteItem(id) {
-     return fetch(`${this._url}/${id}`, {
+     return fetch(`${this.url}/${id}`, {
         method: 'DELETE',
-        headers: this._headers
+        headers: this.headers
      }).then((res) => {
-        return this._processResult(res, 'Ошибка при удалении записи');
+        return this.processResult(res, 'Ошибка при удалении записи');
      });
   }
 
   createItem(data) {
-     return fetch(`${this._url}/`, {
+     return fetch(`${this.url}/`, {
         method: 'POST',
-        headers: this._headers,
+        headers: this.headers,
         body: JSON.stringify(data)
      }).then((res) => {
-        return this._processResult(res, 'Ошибка при добавлении записи');
+        return this.processResult(res, 'Ошибка при добавлении записи');
      });
   }
 
   updateItem(id, data) {
-     return fetch(`${this._url}/${id}`, {
+     return fetch(`${this.url}/${id}`, {
         method: 'PUT',
-        headers: this._headers,
+        headers: this.headers,
         body: JSON.stringify(data)
      }).then((res) => {
-        return this._processResult(res, 'Ошибка при изменении записи');
+        return this.processResult(res, 'Ошибка при изменении записи');
      });
   }
 
-  _processResult(res, errorText) {
+  processResult(res, errorText) {
      if (res.ok) {
         return res.json();
      }
@@ -52,114 +52,186 @@ class Api {
 }
 class Form {
   constructor(element) {
-     this._element = element;
+     this.element = element;
   }
 
   init(submitHandler, values) {
      this.closeForm();
-     this._submitHandler = submitHandler;
-     this._element.addEventListener('submit', this._submitHandler);
+     this.submitHandler = submitHandler;
+     this.element.addEventListener('submit', this.submitHandler);
 
      if (values) {
         Object.keys(values).forEach((name) => {
-           this._element.querySelector(`[name=${name}]`).value = values[name];
+           this.element.querySelector(`[name=${name}]`).value = values[name];
         });
      }
   }
 
   closeForm() {
-     this._element.reset();
-     this._element.removeEventListener('submit', this._submitHandler);
-     this._submitHandler = null;
+     this.element.reset();
+     this.element.removeEventListener('submit', this.submitHandler);
+     this.submitHandler = null;
   }
 }
 
-const $peopleSection = document.querySelector('.people');
-const $personTemplate = document.querySelector('.person-template').content;
-const $popup = document.querySelector('#personPopup');
-const $personButton = document.querySelector('.person-image');
-const $popupCloseButton = document.querySelector('.popup-close');
-const personApi = new Api('http://localhost:5500', { 'Content-Type': 'application/json' });
-const personForm = new Form(document.querySelector('.person-form'));
-
-const showPopup = () => {
-   $popup.classList.add('opened');
+const peopleSection = document.querySelector('.people');
+const personTemplate = document.querySelector('.person-template').content;
+var createPersonButton = document.querySelector('#button-add-mobile');
+if($('#button-add-mobile').css('display') == 'none')
+{
+   createPersonButton = document.querySelector('#button-add-desktop');
 }
 
-const hidePopup = () => {
-   $popup.classList.remove('opened');
-   $personForm.closeForm();
+const personApi = new Api('http://localhost:3000/classmates', { 'Content-Type': 'application/json' });
+const personForm = new Form(document.querySelector('.person-form'));
+
+function showPopup(popup){  
+   var curModal;
+    curModal = popup;
+    $(".modal").each(function() {
+      if (this !== curModal) {
+        $(this).modal('hide')
+      }
+    });
+   popup.modal('show');
+   
+}
+
+function hidePopup(popup){
+   popup.modal('hide')
+   if (popup == $('#person-change'))
+     personForm.closeForm();
 }
 
 const renderList = (data) => {
-   $kittiesSection.innerHTML = '';
+   peopleSection.innerHTML = '';
    data.forEach(renderItem)
 };
 
 const renderItem = (item) => {
-   const $personEl = $personTemplate.cloneNode(true);
-   const $personImg = $personEl.querySelector('.person-image');
-   const $buttonDelete = $personEl.querySelector('.button-delete');
-   const $buttonEdit = $personEl.querySelector('.button-edit');
+   var clicked = false;
+   const personEl = personTemplate.cloneNode(true);
+   const personImg = personEl.querySelector('.person-image');
+   const buttonDelete = personEl.querySelector('.button-delete');
+   
+   const buttonCard = personEl.querySelector('.name');
 
-   $personEl.querySelector('.person-name').textContent = item.name;
-   $personEl.querySelector('.person-info').textContent = item.about;
-   $personImg.setAttribute('src', item.avatarUrl);
-   $personImg.setAttribute('alt', item.name);
+   personEl.querySelector('.first-name').innerHTML = item["first-name"];
+   personEl.querySelector('.last-name').innerHTML = item["last-name"];
+   personEl.querySelector('.study').innerHTML = item.study;
+   personEl.querySelector('.course').innerHTML = item.course + " курс";
+   personEl.querySelector('.city').innerHTML = "г. " + item.city;
+   personImg.setAttribute('src', item.url);
+   
 
-   $kittiesSection.appendChild($personEl);
+   peopleSection.appendChild(personEl);
 
-   $buttonDelete.addEventListener('click', (event) => {
+   buttonDelete.addEventListener('click', (event) => {
       event.preventDefault();
 
       personApi.deleteItem(item.id).then(() => {
          event.target.closest('.person')?.remove?.();
       });
    });
+   
 
-   $buttonEdit.addEventListener('click', (event) => {
-      showPopup();
+   buttonCard.addEventListener('click', () => {
+      
+      const personEl = document.querySelector('.person-card');
+      showPopup($('#person-popup'));
+      personEl.querySelector('.first-name').innerHTML = item["first-name"];
+      personEl.querySelector('.last-name').innerHTML = item["last-name"];
+      personEl.querySelector('.middle-name').innerHTML = item["middle-name"];
+      personEl.querySelector('.study').innerHTML = item.study;
+      personEl.querySelector('.course').innerHTML = item.course + " курс";
+      personEl.querySelector('.city').innerHTML = item.city;
+      personEl.querySelector('.phone').innerHTML = item.phone;
+      personEl.querySelector('.mail').innerHTML = item.mail;
+      personEl.querySelector('.image').setAttribute('src', item.url);
+      personEl.querySelector('.vk-link').setAttribute('href', item.vk);
+      personEl.querySelector('.whatsapp-link').setAttribute('href', item.whatsapp);
+      personEl.querySelector('.facebook-link').setAttribute('href', item.facebook);
+      personEl.querySelector('.telegram-link').setAttribute('href', item.telegram);
+      const buttonEdit = personEl.querySelector('.button-edit');
+
+   buttonEdit.addEventListener('click', (event) => {
+      clicked = true;
+     $('#person-popup').on('hidden.bs.modal', function () {
+      if (clicked)
+         showPopup($('#person-change'));
+         clicked = false;
+    })
+    hidePopup($('#person-popup'));
       personForm.init((event) => {
          event.preventDefault();
          const data = {
             id: item.id,
-            name: event.target.elements[0].value,
-            about: event.target.elements[1].value,
-            avatarUrl: event.target.elements[2].value
+            "last-name": event.target.elements[0].value,
+            "first-name": event.target.elements[1].value,
+            "middle-name": event.target.elements[2].value,
+            phone: event.target.elements[3].value,
+            mail: event.target.elements[4].value,
+            study: event.target.elements[5].value,
+            course: event.target.elements[6].value,
+            city: event.target.elements[7].value,
+            url: event.target.elements[8].value,
+            vk: event.target.elements[9].value,
+            facebook: event.target.elements[10].value,
+            whatsapp: event.target.elements[11].value,
+            telegram: event.target.elements[12].value
          };
 
          personApi.updateItem(item.id, data).then(() => {
             personApi.getItems().then((data) => renderList(data));
-            hidePopup();
+            hidePopup($('#person-change'));
          });
       }, {
-         name: item.name,
-         about: item.about,
-         url: item.avatarUrl
+         "last-name": item["last-name"],
+         "first-name": item["first-name"],
+         "middle-name": item["middle-name"],
+         phone: item.phone,
+         mail: item.mail,
+         study: item.study,
+         course: item.course,
+         city: item.city,
+         url: item.url,
+         vk: item.vk,
+         facebook: item.facebook,
+         whatsapp: item.whatsapp,
+         telegram: item.telegram
       });
+      
    });
+   });
+
 }
 
 
 personApi.getItems().then((data) => renderList(data));
 
-$personButton.addEventListener('click', () => {
-   showPopup();
+createPersonButton.addEventListener('click', () => {
+   showPopup($('#person-change'));
    personForm.init((event) => {
       event.preventDefault();
       const data = {
-         name: event.target.elements[0].value,
-         about: event.target.elements[1].value,
-         avatarUrl: event.target.elements[2].value
+         "last-name": event.target.elements[0].value,
+            "first-name": event.target.elements[1].value,
+            "middle-name": event.target.elements[2].value,
+            phone: event.target.elements[3].value,
+            mail: event.target.elements[4].value,
+            study: event.target.elements[5].value,
+            course: event.target.elements[6].value,
+            city: event.target.elements[7].value,
+            url: event.target.elements[8].value,
+            vk: event.target.elements[9].value,
+            facebook: event.target.elements[10].value,
+            whatsapp: event.target.elements[11].value,
+            telegram: event.target.elements[12].value
       };
 
       personApi.createItem(data).then(() => {
          personApi.getItems().then((data) => renderList(data));
-         hidePopup();
+         hidePopup($('#person-change'));
       });
    });
-});
-
-$popupCloseButton.addEventListener('click', () => {
-   hidePopup();
 });
